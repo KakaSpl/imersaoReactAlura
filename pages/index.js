288604da3f044562f4c21b4d7cfc1b9246c2c627
KakaSpl <cm_domingos@hotmail.com>
2022-01-25 19:09:33 -0300

    segunda aula react alura

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import appConfig from '../config.json'
-import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import appConfig from '../config.json';
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-function GlobalStyle(){
-    return(
-        <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    )
-}
+
 
 function Title(props) {
     const Tag = props.tag || 'h1';
@@ -62,11 +37,14 @@ function HomePage() {
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    // const username = 'peas';
   
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
+        
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +71,11 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                console.log('Alguém submeteu o form');
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,8 +85,24 @@ export default function PaginaInicial() {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
+
+              {/* <input
+                type= "text"
+                value={username}
+                onChange={function (event) {
+                  console.log('usuario digitou', event.target.value);
+                  const valor = event.target.value;
+                  setUsername(valor);
+                }}
+              /> */}
   
               <TextField
+                value={username}
+                onChange={function (event) {
+                  console.log('usuario digitou', event.target.value);
+                  const valor = event.target.value;
+                  setUsername(valor);
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
